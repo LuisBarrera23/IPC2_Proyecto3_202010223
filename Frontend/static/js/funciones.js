7bd19920779443ffb521e1777bbfbf1914376394
LuisBarrera23 <78067073+LuisBarrera23@users.noreply.github.com>
2021-11-03 00:45:25 -0600

    avances

--- Frontend/static/js/funciones.js
+++ Frontend/static/js/funciones.js
@@ -1,17 +1,43 @@
-function cargarArchivo(){
-    var datos=document.getElementById('archivoxml').files[0];
+function cargarArchivo() {
+    var datos = document.getElementById('archivoxml').files[0];
     console.log(datos);
 
-    var scannerxml=new FileReader();
+    var scannerxml = new FileReader();
     scannerxml.readAsText(datos);
     console.log(scannerxml.result);
 
-    scannerxml.onload=function(){
+    scannerxml.onload = function () {
         //console.log(scannerxml.result);
-        contenido=scannerxml.result
+        contenido = scannerxml.result
         // ahora imprimimos en el textarea de entrada
-        document.getElementById('entradaxml').value=contenido
+        document.getElementById('entradaxml').value = contenido
     };
 
-    
 }
+
+function enviararchivo() {
+    texto=document.getElementById('entradaxml').value
+    console.log(texto)
+    var objeto = {
+        'archivo': texto
+    }
+    console.log(objeto)
+
+
+    fetch('http://localhost:5000/registrar', {
+        method: 'POST',
+        body: JSON.stringify(objeto),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        }
+    })
+        .then(res => res.json())
+        .catch(err => {
+            console.error('Error:', err)
+            alert("Ocurrio un error, ver la consola")
+        })
+        .then(response => {
+            console.log(response.Mensaje)
+        })
+};
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask,jsonify,request
+from flask_cors import CORS
 
 app=Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def principal():
-    return '<h1>HOLA MUNDO</h1>'
+@app.route('/registrar',methods=['POST'])
+def registrar():
+    #print(request.json)
+
+    archivo=str(request.json['archivo'])
+    print(archivo)
+    return jsonify({'Mensaje':"Archivo leido"})
+
 
 if __name__=='__main__':
     app.run(debug=True,port=5000)
